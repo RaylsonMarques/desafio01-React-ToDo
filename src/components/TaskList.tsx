03d264a10aab3e8ad1bc476735d9a43b24352e22
Raylson Marques <mr.raylson11@gmail.com>
2021-07-13 10:55:44 -0300

    Refatora função de adicionar tarefa

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -20,8 +20,7 @@ export function TaskList() {
       throw new Error('Title is required');
     }
 
-    tasks.push({ id: Math.random(), title: newTaskTitle, isComplete: false });
-
+    setTasks([...tasks, { id: Math.random(), title: newTaskTitle, isComplete: false }]);
     setNewTaskTitle('');
 
     setTasks(tasks);
