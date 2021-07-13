240e8caedfa4778c58fd13d8cda29d6a10ef5a28
Raylson Marques <mr.raylson11@gmail.com>
2021-07-13 10:55:17 -0300

    Implementa remoção de tarefa

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -33,6 +33,9 @@ export function TaskList() {
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const taskIndex = tasks.findIndex(task => task.id === id);
+    tasks.splice(taskIndex, 1);
+    setTasks([...tasks]);
   }
 
   return (
